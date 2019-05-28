c4a9d20b744920bea570201aebdbe7490641167e
Kevin Zhang <kzhang@federatedwireless.com>
2019-05-28 15:14:53 -0400

    early update

--- src/views/StatsCalc/StatsCol/StatsField/StatsAvatar.js
+++ src/views/StatsCalc/StatsCol/StatsField/StatsAvatar.js
@@ -1,26 +1,17 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import classNames from 'classnames';
-import { refs } from 'store';
 import { connect } from 'react-redux';
-import { Image, withTheme } from 'components';
-import { selectFocus, selectStats, translate, getField } from 'actions';
+import { Image, withTheme } from '../../../../components';
+import {
+  selectFocus,
+  selectStats,
+  translate,
+  getField,
+} from '../../../../actions';
+import { refs } from '../../../../store';
 
 class StatsAvatar extends React.Component {
-  render() {
-    let { lang, image, name, statsKey, highlight } = this.props;
-
-    const field = getField(statsKey);
-    const label = name ? name[lang] : translate(statsKey, lang);
-    const cn = classNames('avatar center', { highlight });
-    return (
-      <div className={cn}>
-        <Image field={field} image={image} size="lg" onClick={this.onClick} />
-        <div className="caption ellipsis">{label}</div>
-      </div>
-    );
-  }
-
   onClick = () => {
     const { highlight, statsKey, selectFocus, selectStats } = this.props;
     if (!highlight) selectStats(statsKey);
@@ -34,6 +25,20 @@ class StatsAvatar extends React.Component {
       });
     }, 0);
   };
+
+  render() {
+    const { lang, image, name, statsKey, highlight } = this.props;
+
+    const field = getField(statsKey);
+    const label = name ? name[lang] : translate(statsKey, lang);
+    const cn = classNames('avatar center', { highlight });
+    return (
+      <div className={cn}>
+        <Image field={field} image={image} size="lg" onClick={this.onClick} />
+        <div className="caption ellipsis">{label}</div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = ({ focusKey, stats }, { statsKey }) => {
